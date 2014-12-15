1afe7ddbbeb1242bc8c634b1e533f0b79a42371c
Mikey Badr <mfbadr@gmail.com>
2014-12-15 14:09:27 -0600

    populate.sql for travis

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -1,1 +1,6 @@
+insert into users (username, password, avatar, token) values ('b', 'b', 'a.png', 'token');
+insert into users (username, password, avatar, token) values ('c', 'b', 'a.png', 'token');
+insert into users (username, password, avatar, token) values ('d', 'b', 'a.png', 'token');
+delete from users;
+
 insert into users (id, username, password, avatar, token) values (1, 'bob', '$2a$08$3oM4O.GL9TGXSqDFoq3ww.62O.tydqdpbBHnuf7xQYwhGGOo9sdzG', 'a.png', 'token');
