84f8b07344519266e91e8335d9aa06968fd7b520
Mikey Badr <mfbadr@gmail.com>
2014-12-16 10:15:36 -0600

    begin acceptance testing

---
+++ test/acceptance/user.js
@@ -0,0 +1,62 @@
+/* jshint expr:true */
+
+'use strict';
+
+var expect       = require('chai').expect,
+    //User         = require('../../server/models/user'),
+    cp           = require('child_process'),
+    server       = require('../../server/index'),
+    Lab          = require('lab'),
+    lab          = exports.lab = Lab.script(),
+    describe     = lab.describe,
+    helpers       = require('../helpers/helpers'),
+    db            = helpers.getDb(),
+    //before       = lab.before,
+    beforeEach   = lab.beforeEach,
+    it           = lab.it;
+
+
+describe('Users', function(){
+  //before(function(done){
+  //});
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../scripts/clean_db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      //console.log('stdout' ,stdout);
+      //console.log('stderr', stderr);
+      done();
+    });
+  });
+  describe('post /register', function(){
+    it('should register a new user', function(done){
+      var options = {
+        method: 'post',
+        url: '/register',
+        payload: {
+          username: 'sam',
+          password: '123',
+          avatar: 'http://png-1.findicons.com/files/icons/1072/face_avatars/300/i04.png'
+        }
+      };
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+  describe('post /login', function(){
+    it('should login a user', function(done){
+      var options = {
+        method: 'post',
+        url: '/login',
+        payload: {
+          username: 'bob',
+          password: 'bob'
+        }
+      };
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+});
