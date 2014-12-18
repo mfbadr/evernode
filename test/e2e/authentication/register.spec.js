df54fb18279a0b7210394322b145de82df9d0530
Mikey Badr <mfbadr@gmail.com>
2014-12-18 11:07:32 -0600

    init e2e for register

--- test/e2e/authentication/register.spec.js
+++ test/e2e/authentication/register.spec.js
@@ -15,4 +15,13 @@ describe('register', function(){
     browser.get('/#/register');
     expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('register');
   });
+
+  it('should register a new user', function(){
+    element(by.model('user.username')).sendKeys('sam');
+    element(by.model('user.password')).sendKeys('456');
+    element(by.model('user.avatar')).sendKeys('http://png-1.findicons.com/files/icons/1072/face_avatars/300/i04.png');
+    expect(element(by.css('button[ng-click="submit()"]')).click();
+
+
+  });
 });
