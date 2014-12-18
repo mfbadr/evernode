454f45a38dd887b0ed787092e999d9a66f592ef9
Mikey Badr <mfbadr@gmail.com>
2014-12-18 11:27:06 -0600

    e2e login page init

---
+++ test/e2e/authentication/login.spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var cp = require('child_process'),
+    h  = require('../../helpers/helpers'),
+    db = h.getDb();
+
+describe('login', function(){
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../../scripts/clean_db.sh', [db], {cwd:__dirname + '/../../scripts'}, function(err, stdout, stderr){
+      browser.get('/#/login');
+      done();
+    });
+  });
+
+  it('should get login page', function(){
+    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('login');
+  });
+
+  /*
+  it('should register a new user', function(){
+    element(by.model('user.username')).sendKeys('sam' + h.random(5000));
+    element(by.model('user.password')).sendKeys('456');
+    element(by.model('user.avatar')).sendKeys('http://png-1.findicons.com/files/icons/1072/face_avatars/300/i04.png');
+    element(by.css('button[ng-click="submit()"]')).click();
+    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('login');
+  });
+  */
+});
